6599ad8a2b2ae826371ba23ffe639fea4c1a986c
Corey Ford <corey@coreyford.name>
2014-04-14 14:53:55 +0200

    Add Bike and Station nodes and relationships as necessary

--- graph-db-driver/src/main/java/se/kth/csc/moderndb/cbexplorer/domain/CitiBikeGraphService.java
+++ graph-db-driver/src/main/java/se/kth/csc/moderndb/cbexplorer/domain/CitiBikeGraphService.java
@@ -5,6 +5,8 @@ import org.springframework.data.neo4j.support.Neo4jTemplate;
 import org.springframework.transaction.annotation.Transactional;
 import se.kth.csc.moderndb.cbexplorer.parser.CitiBikeParser;
 import se.kth.csc.moderndb.cbexplorer.parser.CitiBikeReader;
+import se.kth.csc.moderndb.cbexplorer.parser.data.BikeData;
+import se.kth.csc.moderndb.cbexplorer.parser.data.StationData;
 import se.kth.csc.moderndb.cbexplorer.parser.data.TripData;
 
 import java.io.File;
@@ -33,10 +35,44 @@ public class CitiBikeGraphService {
             for (TripData tripData : trips) {
                 Trip trip = new Trip(tripData.getStartTime(), tripData.getEndTime(), tripData.getUserType(),
                         tripData.getUserBirthYear(), tripData.getUserGenderAsString());
-                Bike bike; // TODO find or create these nodes as necessary
-                Station startStation, endStation;
+                Bike bike = obtainBike(tripData.getBikeData());
+                Station startStation = obtainStation(tripData.getStartStationData());
+                Station endStation = obtainStation(tripData.getEndStationData());
+
+                trip.startedFrom = startStation;
+                trip.endedAt = endStation;
+                trip.bike = bike;
                 template.save(trip);
             }
+        }
+
+        /**
+         * Find or create a Bike instance corresponding to the data.
+         * @param bikeData
+         * @return new or existing Bike with a matching id
+         */
+        private Bike obtainBike(BikeData bikeData) {
+            Bike bike = template.findUniqueEntity(Bike.class, "id", bikeData.getId());
+            if (bike == null) {
+                bike = new Bike(bikeData.getId());
+                template.save(bike);
+            }
+            return bike;
+        }
+
+        /**
+         * Find or create a Station instance corresponding to the data.
+         * @param stationData
+         * @return new or existing Station with a matching stationId
+         */
+        private Station obtainStation(StationData stationData) {
+            Station station = template.findUniqueEntity(Station.class, "stationId", stationData.getStationId());
+            if (station == null) {
+                station = new Station(stationData.getStationId(), stationData.getName(),
+                        stationData.getLongitude(), stationData.getLatitude());
+                template.save(station);
+            }
+            return station;
         }
     }
 }
