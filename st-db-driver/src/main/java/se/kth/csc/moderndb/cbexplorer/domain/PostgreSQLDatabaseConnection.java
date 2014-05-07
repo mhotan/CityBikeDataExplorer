30c5c3113726422650be5a5f209dbba33770504d
Jeannine <jeannine.endress@stud.tu-darmstadt.de>
2014-05-07 14:42:06 +0200

    Change in table structure. Just two tables now and a primary key consisting of bike id, start time for the trip

--- st-db-driver/src/main/java/se/kth/csc/moderndb/cbexplorer/domain/PostgreSQLDatabaseConnection.java
+++ st-db-driver/src/main/java/se/kth/csc/moderndb/cbexplorer/domain/PostgreSQLDatabaseConnection.java
@@ -1,12 +1,13 @@
 package se.kth.csc.moderndb.cbexplorer.domain;
 
-import se.kth.csc.moderndb.cbexplorer.data.TripDataObject;
 import se.kth.csc.moderndb.cbexplorer.parser.data.StationData;
+import se.kth.csc.moderndb.cbexplorer.parser.data.TripData;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.Statement;
+import java.util.Collection;
 import java.util.HashSet;
 
 /**
@@ -19,10 +20,7 @@ public class PostgreSQLDatabaseConnection {
 
     // names of the tables that will be created
     public static final String STATION = "station";
-    public static final String TRIPTIME = "trip_time";
-    public static final String TRIPROUTE = "trip_route";
-    public static final String TRIPSETTS = "trip_settings";
-
+    public static final String TRIP = "trip";
     public static final String URL = "jdbc:postgresql://localhost:5432/";
 
     public static final String DATABASE_NAME = "citybike";
@@ -90,30 +88,6 @@ public class PostgreSQLDatabaseConnection {
         }
     }
 
-    /**
-     * Creates the database table TRIP_TIME having the following structure:
-     * ID (int, primary key), START_TIME (int), END_TIME (int).
-     * It also adds a rule to ignore duplicated entries in the database.
-     *
-     * @param c connection to the database
-     */
-    public void createTRIPTIMETable(Connection c) {
-        try {
-            Statement stmt = c.createStatement();
-            String sql = "CREATE TABLE IF NOT EXISTS " + TRIPTIME + " " +
-                    "(" + ID + " BIGINT PRIMARY KEY    NOT NULL," +
-                    " " + STARTTIME + " DATE    NOT NULL, " +
-                    " " + ENDTIME + " DATE  NOT NULL)";
-            stmt.executeUpdate(sql);
-            String rule = "CREATE OR REPLACE RULE \"trip_time_on_duplicate_ignore\" AS ON INSERT TO \"" + TRIPTIME + "\" WHERE EXISTS(SELECT 1 FROM " + TRIPTIME + " WHERE (" + ID + ")=(NEW." + ID + ")) DO INSTEAD NOTHING;";
-            stmt.execute(rule);
-            stmt.close();
-            //c.close();
-        } catch (Exception e) {
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-            System.exit(0);
-        }
-    }
 
     /**
      * Creates the database table TRIP_ROUTE having the following structure:
@@ -125,41 +99,20 @@ public class PostgreSQLDatabaseConnection {
     public void createTRIPROUTETable(Connection c) {
         try {
             Statement stmt = c.createStatement();
-            String sql = "CREATE TABLE IF NOT EXISTS " + TRIPROUTE + " " +
-                    "(" + ID + " BIGINT PRIMARY KEY     NOT NULL," +
+            String sql = "CREATE TABLE IF NOT EXISTS " + TRIP + " " +
+                    "(" + BIKEID + " BIGINT      NOT NULL," +
+                    " " + STARTTIME + " DATE  NOT NULL, " +
+                    " " + ENDTIME + " DATE  NOT NULL," +
                     " " + STARTSTATION + " BIGINT  NOT NULL, " +
-                    " " + ENDSTATION + " BIGINT    NOT NULL)";
-            stmt.executeUpdate(sql);
-            String rule = "CREATE OR REPLACE RULE \"trip_route_on_duplicate_ignore\" AS ON INSERT TO \"" + TRIPROUTE + "\" WHERE EXISTS(SELECT 1 FROM " + TRIPROUTE + " WHERE (" + ID + ")=(NEW." + ID + ")) DO INSTEAD NOTHING;";
-            stmt.execute(rule);
-
-            stmt.close();
-            //c.close();
-        } catch (Exception e) {
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-            System.exit(0);
-        }
-    }
-
-    /**
-     * Creates the database table TRIP_SETTINGS having the following structure:
-     * ID (int, primary key), BIKE_ID (int), USER_TYPE (text), GENDER (int).
-     * It also adds a rule to ignore duplicated entries in the database.
-     *
-     * @param c connection to the database
-     */
-    public void createTRIPSETTINGSTable(Connection c) {
-        try {
-            Statement stmt = c.createStatement();
-            String sql = "CREATE TABLE IF NOT EXISTS " + TRIPSETTS + " " +
-                    "(" + ID + " BIGINT PRIMARY KEY    NOT NULL," +
-                    " " + BIKEID + " BIGINT    NOT NULL, " +
+                    " " + ENDSTATION + " BIGINT    NOT NULL," +
                     " " + USERTYPE + " TEXT NOT NULL, " +
                     " " + BIRTHYEAR + " INT NOT NULL, " +
-                    " " + GENDER + " INT    NOT NULL)";
+                    " " + GENDER + " INT    NOT NULL," +
+                    " PRIMARY KEY(" + BIKEID +","+ STARTTIME+"))";
             stmt.executeUpdate(sql);
-            String rule = "CREATE OR REPLACE RULE \"trip_setts_on_duplicate_ignore\" AS ON INSERT TO \"" + TRIPSETTS + "\" WHERE EXISTS(SELECT 1 FROM " + TRIPSETTS + " WHERE (" + ID + ")=(NEW." + ID + ")) DO INSTEAD NOTHING;";
+            String rule = "CREATE OR REPLACE RULE \"trip_route_on_duplicate_ignore\" AS ON INSERT TO \"" + TRIP + "\" WHERE EXISTS(SELECT 1 FROM " + TRIP + " WHERE (" + BIKEID + "," + STARTTIME + ")=(NEW." + BIKEID + ", NEW." + STARTTIME + ")) DO INSTEAD NOTHING;";
             stmt.execute(rule);
+
             stmt.close();
             //c.close();
         } catch (Exception e) {
@@ -167,20 +120,18 @@ public class PostgreSQLDatabaseConnection {
             System.exit(0);
         }
     }
+
+
 
     /**
      * Creates the tables STATION {@link #createSTATIONTable(java.sql.Connection)},
-     * TRIP_ROUTE {@link #createTRIPROUTETable(java.sql.Connection)},
-     * TRIP_TIME {@link #createTRIPTIMETable(java.sql.Connection)} and
-     * TRIP_SETTINGS {@link #createTRIPSETTINGSTable(java.sql.Connection)}.
+     * TRIP_ROUTE {@link #createTRIPROUTETable(java.sql.Connection)}.
      *
      * @param c connection to the database
      */
     public void createAllNecessaryTables(Connection c) {
         createSTATIONTable(c);
         createTRIPROUTETable(c);
-        createTRIPTIMETable(c);
-        createTRIPSETTINGSTable(c);
     }
 
     /**
@@ -210,48 +161,28 @@ public class PostgreSQLDatabaseConnection {
         }
     }
 
+
+
     /**
-     * Inserts table specific data from {@param #tripDataObjects} into the table TRIP_TIME {@link #createTRIPTIMETable(java.sql.Connection)}.
+     * Inserts table specific data from {@param #tripDate} into the table TRIP_ROUTE {@link #createTRIPROUTETable(java.sql.Connection)}.
      *
      * @param c               connection to the database
-     * @param tripDataObjects set containing data of the given trips
+     * @param tripDate set containing data of the given trips
      */
-    public void insertIntoTRIPTIME(Connection c, HashSet<TripDataObject> tripDataObjects) {
+    public void insertIntoTRIP(Connection c, Collection<TripData> tripDate) {
         try {
-            String sql = "INSERT INTO " + TRIPTIME + " (" + ID + "," + STARTTIME + "," + ENDTIME + ") "
-                    + "VALUES (?, ?, ?)";
+            String sql = "INSERT INTO " + TRIP + " (" + BIKEID + "," + STARTTIME + "," + ENDTIME + "," + STARTSTATION + "," + ENDSTATION + "," + USERTYPE +"," + BIRTHYEAR + "," + GENDER+ ") "
+                    + "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
             PreparedStatement preparedStatement = c.prepareStatement(sql);
 
-            for (TripDataObject tripDataObject : tripDataObjects) {
-                preparedStatement.setLong(1, tripDataObject.getTripID());
+            for (TripData tripDataObject : tripDate) {
+                preparedStatement.setLong(1, tripDataObject.getBikeData().getId());
                 preparedStatement.setDate(2, new java.sql.Date(tripDataObject.getStartTime().getTime()));
-                preparedStatement.setDate(3, new java.sql.Date(tripDataObject.getEndTime().getTime()));
-                preparedStatement.addBatch();
-            }
-            preparedStatement.executeBatch();
-        } catch (Exception e) {
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-            System.exit(0);
-        }
-
-    }
-
-    /**
-     * Inserts table specific data from {@param #tripDataObjects} into the table TRIP_ROUTE {@link #createTRIPROUTETable(java.sql.Connection)}.
-     *
-     * @param c               connection to the database
-     * @param tripDataObjects set containing data of the given trips
-     */
-    public void insertIntoTRIPROUTE(Connection c, HashSet<TripDataObject> tripDataObjects) {
-        try {
-            String sql = "INSERT INTO " + TRIPROUTE + " (" + ID + "," + STARTSTATION + "," + ENDSTATION + ") "
-                    + "VALUES (?, ?, ?)";
-            PreparedStatement preparedStatement = c.prepareStatement(sql);
-
-            for (TripDataObject tripDataObject : tripDataObjects) {
-                preparedStatement.setLong(1, tripDataObject.getTripID());
-                preparedStatement.setLong(2, tripDataObject.getStartStationID());
-                preparedStatement.setLong(3, tripDataObject.getEndStationID());
+                preparedStatement.setLong(3, tripDataObject.getStartStationData().getStationId());
+                preparedStatement.setLong(4, tripDataObject.getEndStationData().getStationId());
+                preparedStatement.setString(5, tripDataObject.getUserType());
+                preparedStatement.setInt(6, tripDataObject.getUserBirthYear());
+                preparedStatement.setInt(7, tripDataObject.getUserGender());
                 preparedStatement.addBatch();
             }
             preparedStatement.executeBatch();
@@ -261,33 +192,7 @@ public class PostgreSQLDatabaseConnection {
         }
     }
 
-    /**
-     * Inserts table specific data from {@param #tripDataObjects} into the table TRIP_SETTINGS {@link #createTRIPSETTINGSTable(java.sql.Connection)}
-     *
-     * @param c               connection to the database
-     * @param tripDataObjects set containing data of the given trips
-     */
-    public void insertIntoTRIPSETTINGS(Connection c, HashSet<TripDataObject> tripDataObjects) {
-        try {
-            String sql = "INSERT INTO " + TRIPSETTS + " (" + ID + "," + BIKEID + "," + USERTYPE + "," + BIRTHYEAR + "," +  GENDER + ") "
-                    + "VALUES (?, ?, ?, ?, ?)";
-            PreparedStatement preparedStatement = c.prepareStatement(sql);
 
-            for (TripDataObject tripDataObject : tripDataObjects) {
-                preparedStatement.setLong(1, tripDataObject.getTripID());
-                preparedStatement.setLong(2, tripDataObject.getBikeID());
-                preparedStatement.setString(3, tripDataObject.getUserDescription());
-                preparedStatement.setInt(4, tripDataObject.getBirthyear());
-                preparedStatement.setInt(5, tripDataObject.getGender());
-                preparedStatement.addBatch();
-            }
-            preparedStatement.executeBatch();
-
-        } catch (Exception e) {
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-            System.exit(0);
-        }
-    }
 
 
     /*private void creatingInitDatabase() {
