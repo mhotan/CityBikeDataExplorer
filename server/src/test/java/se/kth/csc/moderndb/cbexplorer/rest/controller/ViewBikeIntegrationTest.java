47d17c538359a4b819f8b8fe51be2ac04ecd99f6
Michael Hotan <michael.hotan@gmail.com>
2014-05-07 16:23:37 +0200

    Initial Test for Bike Rest Integration

---
+++ server/src/test/java/se/kth/csc/moderndb/cbexplorer/rest/controller/ViewBikeIntegrationTest.java
@@ -0,0 +1,7 @@
+package se.kth.csc.moderndb.cbexplorer.rest.controller;
+
+/**
+ * Created by mhotan on 5/7/14.
+ */
+public class ViewBikeIntegrationTest {
+}
--- server/src/test/java/se/kth/csc/moderndb/cbexplorer/rest/controller/fixture/RestDataFixture.java
+++ server/src/test/java/se/kth/csc/moderndb/cbexplorer/rest/controller/fixture/RestDataFixture.java
@@ -1,10 +1,36 @@
 package se.kth.csc.moderndb.cbexplorer.rest.controller.fixture;
 
+import se.kth.csc.moderndb.cbexplorer.core.domain.Bike;
+import se.kth.csc.moderndb.cbexplorer.core.events.bikes.AllBikesEvent;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
+ * Testing data fixture.
+ *
  * Created by mhotan on 5/7/14.
  */
 public class RestDataFixture {
 
     public static final long TEST_ID = 1L;
+
+    public static AllBikesEvent allBikes() {
+        List<Bike> bikes = new ArrayList<Bike>();
+        bikes.add(standardBike());
+        bikes.add(standardBike());
+        bikes.add(standardBike());
+        return new AllBikesEvent(bikes);
+    }
+
+    public static Bike standardBike() {
+        return new Bike(TEST_ID);
+    }
+
+    public static Bike customBike(long id) {
+        return new Bike(id);
+    }
+
+
 
 }
