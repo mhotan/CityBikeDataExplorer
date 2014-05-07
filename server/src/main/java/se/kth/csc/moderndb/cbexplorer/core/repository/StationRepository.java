d96f0a78180e86bd1fa2184e759c22d269cad91d
Corey Ford <corey@coreyford.name>
2014-05-07 13:06:10 +0200

    Some custom queries on StationRepository

--- server/src/main/java/se/kth/csc/moderndb/cbexplorer/core/repository/StationRepository.java
+++ server/src/main/java/se/kth/csc/moderndb/cbexplorer/core/repository/StationRepository.java
@@ -1,11 +1,23 @@
 package se.kth.csc.moderndb.cbexplorer.core.repository;
 
+import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.GraphRepository;
+import org.springframework.data.repository.query.Param;
 import se.kth.csc.moderndb.cbexplorer.domain.Station;
+
+import java.util.List;
+
+import static se.kth.csc.moderndb.cbexplorer.DatabaseConstants.*;
 
 /**
  * Created by mhotan on 4/21/14.
  */
 public interface StationRepository extends GraphRepository<Station> {
+
+    Station findByName(@Param("0") String name);
+
+    @Query("START start=node({station}) MATCH start<-[:" + STARTED_FROM_RELATION + "]-" +
+            "trip-[:" + ENDED_AT_RELATION + "]->end RETURN DISTINCT end")
+    List<Station> getDestinations(@Param("station") Long startStationId);
 
 }
