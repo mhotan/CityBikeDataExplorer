e2d2962c353c06eb816836ab04a6933e72eac6f6
Michael Hotan <michael.hotan@gmail.com>
2014-05-07 16:24:08 +0200

    Initial Test for Bike Rest integration

--- server/src/test/java/se/kth/csc/moderndb/cbexplorer/rest/controller/ViewBikeIntegrationTest.java
+++ server/src/test/java/se/kth/csc/moderndb/cbexplorer/rest/controller/ViewBikeIntegrationTest.java
@@ -1,7 +1,84 @@
 package se.kth.csc.moderndb.cbexplorer.rest.controller;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.test.web.servlet.MockMvc;
+import se.kth.csc.moderndb.cbexplorer.core.events.bikes.RequestBikeEvent;
+import se.kth.csc.moderndb.cbexplorer.core.services.BikeService;
+import se.kth.csc.moderndb.cbexplorer.rest.RestContants;
+
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static se.kth.csc.moderndb.cbexplorer.rest.controller.fixture.RestEventFixtures.*;
+
 /**
  * Created by mhotan on 5/7/14.
  */
 public class ViewBikeIntegrationTest {
+
+    // The Mock Object
+    MockMvc mockMvc;
+
+    @InjectMocks
+    BikeQueriesController controller;
+
+    @Mock
+    BikeService bikeService;
+
+    long testId = 1l;
+
+    @Before
+    public void setUp() throws Exception {
+        // Initialize the Mock tests
+        MockitoAnnotations.initMocks(this);
+
+        this.mockMvc = standaloneSetup(controller).setMessageConverters(
+                new MappingJackson2HttpMessageConverter()).build();
+    }
+
+    @Test
+    public void thatViewOrderUsesHttpNotFound() throws Exception {
+
+        when(bikeService.requestBike(any(RequestBikeEvent.class))).thenReturn(
+                bikeNotFound(testId));
+
+        this.mockMvc.perform(
+                get(RestContants.AGGREGATORS_URI_PATH + RestContants.BIKES_URI_PATH + "/{id}", testId)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void thatViewOrderUsesHttpOK() throws Exception {
+
+        when(bikeService.requestBike(any(RequestBikeEvent.class))).thenReturn(
+                bikeEvent(testId));
+
+        this.mockMvc.perform(
+                get(RestContants.AGGREGATORS_URI_PATH + RestContants.BIKES_URI_PATH + "/{id}", testId)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void thatViewOrderRendersCorrectly() throws Exception {
+
+        when(bikeService.requestBike(any(RequestBikeEvent.class))).thenReturn(
+                bikeEvent(testId));
+
+        this.mockMvc.perform(
+                get(RestContants.AGGREGATORS_URI_PATH + RestContants.BIKES_URI_PATH + "/{id}", testId)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.bikeId").value(testId));
+    }
 }
--- server/src/test/java/se/kth/csc/moderndb/cbexplorer/rest/controller/fixture/RestEventFixtures.java
+++ server/src/test/java/se/kth/csc/moderndb/cbexplorer/rest/controller/fixture/RestEventFixtures.java
@@ -2,18 +2,16 @@ package se.kth.csc.moderndb.cbexplorer.rest.controller.fixture;
 
 import se.kth.csc.moderndb.cbexplorer.core.events.bikes.BikeEvent;
 
-import java.util.UUID;
-
 /**
  * Created by mhotan on 5/7/14.
  */
 public class RestEventFixtures {
 
-    public static BikeEvent orderDetailsNotFound(long bikeId) {
+    public static BikeEvent bikeNotFound(long bikeId) {
         return BikeEvent.notFound(bikeId);
     }
-    public static BikeEvent orderDetailsEvent(long bikeId) {
-        return new BikeEvent(bikeId, customKeyOrderDetails(key));
+    public static BikeEvent bikeEvent(long bikeId) {
+        return new BikeEvent(RestDataFixture.customBike(bikeId));
     }
 
 }
