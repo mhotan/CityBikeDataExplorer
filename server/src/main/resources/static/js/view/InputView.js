23f76619bc1dfd2a2c0471c1a410c5f09b43343e
Jeannine <jeannine.endress@stud.tu-darmstadt.de>
2014-05-12 11:56:47 +0200

    input view and controller partly implemented

--- server/src/main/resources/static/js/view/InputView.js
+++ server/src/main/resources/static/js/view/InputView.js
@@ -12,4 +12,54 @@ var InputView = function(container, model) {
     this.model = model;
 
 
+    // stationInfo can either be a name or an ID
+    var searchStation = function(stationInfo){
+        // TODO call query
+    }
+
+    var showStations = function(results){
+        this.list = container.find("#stationResults");
+        // TODO fill list
+    }
+
+    var searchBike = function(bikeID){
+        // TODO call query
+    }
+
+    var showBikes = function(results){
+        this.list = container.find("#bikeResults");
+        // TODO fill list
+    }
+
+    var setTimeRangeForStations(minDate, maxDate){
+    // Set date option
+        $("#slider-range1").dateRangeSlider(
+          "bounds":
+              {
+                 min: minDate,
+                  max: maxDate
+               });
+    }
+
+    var setTimeRangeForBikes(minDate, maxDate){
+    // Set date option
+        $("#slider-range2").dateRangeSlider(
+          "bounds":
+          {
+            min: minDate,
+            max: maxDate
+        });
+    }
+
+
+    // Register this view as an observer to the model.
+    model.addObserver(this);
+
+    // This method gets called when the model notifies the observers
+    this.update = function(arg) {
+        // TODO Update the Input view by the current state of the model.
+
+    }
+
 }
+
